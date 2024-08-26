e2ce638c329d71bbe0ec003edc61654f437d25c6
Daniel Wulff <danielwulff.ee@gmail.com>
2024-08-26 16:22:06 +0100

    Create initial preact island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-tutorial-7231.netlify.app/"
+  site: "https://astro-tutorial-7231.netlify.app/",
+  integrations: [preact()]
 });
